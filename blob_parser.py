0d48b296bfec612609eda41cff0b369187677bf6
ramanath <varun16112@iiitd.ac.in>
2019-05-16 16:58:47 +0200

    started coding in blob_parser. imported networkx and ElementTree.

--- blob_parser.py
+++ blob_parser.py
@@ -1,4 +1,9 @@
 import networkx as nx
+import xml.etree.ElementTree as ET
 
 G = nx.DiGraph()
+net_tree = ET.parse('blob_merge_prepacked.net')
+place_tree = ET.parse('blob_merge_prepackaged.place')
+net_root = net_tree.getroot()
+place_root = place_tree.getroot()
 
