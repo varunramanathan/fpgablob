f89990b15515de158a3b1d4e637edc237efc9ecc
ramanath <varun16112@iiitd.ac.in>
2019-05-17 14:42:45 +0200

    created add_nodes_recursive function which adds nodes to the DiGraph recursively from the .place file. NOTE: only adds nodes with names and type (lut or ff)

--- blob_parser.py
+++ blob_parser.py
@@ -1,9 +1,39 @@
 import networkx as nx
 import xml.etree.ElementTree as ET
 
+def add_nodes_recursive(graph, xml_node):
+    """
+    Checks whether xml_node has 'mode' attribute as 'ble'. If yes, it creates a node in G
+    for this element, with name of the node as 'name' attribute of the xml node's child and type attribute
+    as the 'mode' attribute of the xml child.
+    :param graph: DiGraph to which we will add ble elements of the circuit as nodes and connections as edges
+    :param xml_node: the current node in the xml tree
+    """
+    if "mode" in xml_node.attrib and xml_node.attrib["mode"] == "ble":
+        for child in xml_node:
+            name_exists = "name" in child.attrib
+            if name_exists:
+                not_open = child.attrib["name"] != "open"
+                is_ff = (child.attrib['instance'])[:2] == "ff"
+                is_lut = (child.attrib['instance'])[:3] == "lut"
+                if not_open and (is_ff or is_lut):
+                    node_name = child.attrib["name"]
+                    graph.add_node(node_name)
+                    # print(child.attrib)
+                    if is_ff:
+                        graph.nodes[node_name]['type'] = "ff"
+                    else:
+                        graph.nodes[node_name]['type'] = "lut"
+    else:
+        for child_node in xml_node:
+            add_nodes_recursive(graph, child_node)
+
+
 G = nx.DiGraph()
 net_tree = ET.parse('blob_merge_prepacked.net')
-place_tree = ET.parse('blob_merge_prepackaged.place')
 net_root = net_tree.getroot()
-place_root = place_tree.getroot()
-
+# for child in net_root:
+#     print(child.tag, child.attrib)
+add_nodes_recursive(G, net_root)
+for node in G.nodes:
+    print(str(node)+" "+G.nodes[node]['type'])
